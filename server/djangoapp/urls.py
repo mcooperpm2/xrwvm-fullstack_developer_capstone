4d3f7eec07beed0d9ba572f68aa48fe48ad49fa6
Michael Cooper <mcooperpm2@gmail.com>
2026-08-21 17:04:15 -0400

    Create Django Views to Get Dealers

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -18,8 +18,16 @@ urlpatterns = [
     # path for get cars
     path(route='get_cars', view=views.get_cars, name='getcars'),
     
-    # path for dealer reviews view
+    # paths for get dealerships
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
 
+    # paths for get dealer details
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    
+    # path for dealer reviews view
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
