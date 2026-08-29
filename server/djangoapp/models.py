a69fd306c105f5b52adc7c179793659f7879a9f6
mcooperpm2 <mcooperpm2@gmail.com>
2026-08-29 18:30:41 -0400

    Update models.py
    Removed "from django.utils.timezone import now", added new blank line above "class CarModel(models.Model):", and added a new blank line (newline) at the end of the file.

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,6 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -30,6 +29,7 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
+
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
@@ -56,3 +56,4 @@ class CarModel(models.Model):
 
     def __str__(self):
         return self.name
+        
