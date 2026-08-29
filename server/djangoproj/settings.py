65ab44efc8519310c2aa8fd9f43a4ccc89c96791
mcooperpm2 <mcooperpm2@gmail.com>
2026-08-29 19:37:38 -0400

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,20 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','https://mcooperpm2-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://mcooperpm2-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = [
+    'localhost',
+    (
+        'https://mcooperpm2-8000.theianext-0-labs-prod-misc-tools-'
+        'us-east-0.proxy.cognitiveclass.ai'
+    ),
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    (
+        'https://mcooperpm2-8000.theiadockernext-0-labs-prod-'
+        'theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    ),
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -93,8 +105,10 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
+        ),
     },
     {
         'NAME':
@@ -143,4 +157,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-
