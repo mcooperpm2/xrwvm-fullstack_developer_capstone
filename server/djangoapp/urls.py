ba1f05f778f8e0e8f69c79bc7a36b9b3fc497214
mcooperpm2 <mcooperpm2@gmail.com>
2026-08-29 19:08:01 -0400

    Update urls.py
    Shortened long lines, and removed unnecessary blank spaces.

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -4,11 +4,13 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
+
 app_name = 'djangoapp'
+
 urlpatterns = [
     # # path for registration
     path(route='register', view=views.registration, name='register'),
-    
+
     # path for login
     path(route='login', view=views.login_user, name='login'),
 
@@ -17,18 +19,34 @@ urlpatterns = [
 
     # path for get cars
     path(route='get_cars', view=views.get_cars, name='getcars'),
-    
+
     # paths for get dealerships
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(
+        route='get_dealers',
+        view=views.get_dealerships,
+        name='get_dealers',
+    ),
+    path(
+        route='get_dealers/<str:state>',
+        view=views.get_dealerships,
+        name='get_dealers_by_state',
+    ),
 
     # paths for get dealer details
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    
+    path(
+        route='dealer/<int:dealer_id>',
+        view=views.get_dealer_details,
+        name='dealer_details',
+    ),
+
     # path for dealer reviews view
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
-    
+    path(
+        route='reviews/dealer/<int:dealer_id>',
+        view=views.get_dealer_reviews,
+        name='dealer_details',
+    ),
+
     # path for add a review view
     path(route='add_review', view=views.add_review, name='add_review'),
-    
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
